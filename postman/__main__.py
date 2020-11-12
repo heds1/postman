122c698a0640f0b5cde39204fa87298935d4af65
heds1 <hedley.stirrat@gmail.com>
2020-11-12 21:05:54 +1300

    chore: implement Blog class
    initial work at more structured workflow, including more intelligent diffing by parsing HTML a tags and looking for new tags. If there are new tags, it is intended that these are opened rather than the home page if possible.

--- postman/__main__.py
+++ postman/__main__.py
@@ -2,9 +2,10 @@ import requests
 import json
 from webbrowser import open as wbo
 from datetime import datetime
+from bs4 import BeautifulSoup
 
 # define blog URL endpoints to search
-blog_urls = {
+blogs = {
     'megan_stodel': 'https://www.meganstodel.com/',
     'tunnelsup': 'https://www.tunnelsup.com/blog/archives/',
     'data_colada': 'http://www.datacolada.org/',
@@ -12,6 +13,45 @@ blog_urls = {
     'rachel_by_the_bay': "https://rachelbythebay.com/",
 }
 
+class Blog:
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+        self.links = None
+        self.update = False
+
+    def parse_links(self):
+        """
+        Get the raw HTML from the blog's homepage URL,
+        and parse links found in all <a> tags.
+        """
+        try:
+            page = requests.get(self.url).content.decode()
+        except:
+            print("Could not retrieve content from " + self.name)
+            return None
+        
+        try:
+            soup = BeautifulSoup(page, 'html.parser')
+            self.links = []
+            for link in soup.find_all('a'):
+                print(link.get('href'))
+                self.links.append(link.get('href'))
+            self.links.sort()
+            return
+        except:
+            print("Could not parse HTML a tags.")
+            return None
+    
+    def compare_links(self, reference_links):
+        # if the links are different, find the differences
+        if blog.links != reference_links.sort():
+            new_links = set(blog.links) - set(reference_links.sort())
+
+        else:
+            # no updated content
+            return None
+            
 # import previous webpage content
 try:
     with open('old_content.json', 'r') as f:
@@ -22,11 +62,18 @@ except FileNotFoundError as e:
     'this is fine.')
     first_time = True
 
-# get current webpage content
 new_content = {}
-for i in blog_urls:
-    page = requests.get(blog_urls[i])
-    new_content[i] = page.content.decode()
+
+for i in blogs:
+    blog = Blog(name=i, url=blogs[i])
+    blog.parse_links()
+    new_content[i] = blog.links
+
+    blog.compare_links(new_content[i])
+
+
+
+    links_dict[i] = blog.links
 
 if 'first_time' not in locals():
     # if any new content is found that does not
